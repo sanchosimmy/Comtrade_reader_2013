5ff34b209cec30e19669daf765852198efc89181
sanchosimmy <sanchoatiitb@gmail.com>
2021-02-18 18:38:22 +0530

    Analog working separate plots

--- test.py
+++ test.py
@@ -1,13 +1,15 @@
+from tkinter import Tk
 from tkinter import filedialog as fd
 import math
 import struct
 import matplotlib.pyplot as plt
 import numpy as np
+Tk().withdraw()
 print("Sancho's COMTRADE reader")
 filename = fd.askopenfilename(initialdir='')
 tempstring=filename.split('.')
 datfile=tempstring[0]+".dat"
-cfgfile=filename
+cfgfile=tempstring[0]+".cfg"
 f = open(cfgfile, "rt")
 pmu=f.readline().split(',')
 pmu_name=pmu[0]
@@ -21,27 +23,32 @@ total=int(pmu[0])
 temp=pmu[1].split('A')
 tempstring=temp[0]
 analog=int(tempstring)
-print(total)
-print(analog)
+print(f"Total channels = {total}")
+print(f"Total Analog channels = {analog}")
 digital=total-analog
+print(f"Total Analog channels = {digital}")
 analogchannelname=[];
 digitalchannelname=[];
 unit=[];
+print("Analog channels are : ")
 for x in range(0,analog):
     pmu=f.readline().split(',')
     analogchannelname.append(pmu[1])
+    print(pmu[1])
     unit.append(pmu[4])
+print("Digital channels are : ")
 for x in range(0,digital):
     pmu=f.readline().split(',')
     digitalchannelname.append(pmu[1])
-print(analogchannelname)
-print(digitalchannelname)
+    print(pmu[1])
+# print(analogchannelname)
+# print(digitalchannelname)
 pmu=f.readline()
 pmu=f.readline()
 pmu=f.readline().split(',')
 tempstring=pmu[1]
 samples=int(tempstring);
-print(samples)
+print(f"Total Number of samples = {samples}")
 f.close()
 f = open(datfile,"rb")
 data=[]
@@ -60,48 +67,27 @@ for y in range(0,samples):
         ans=int.from_bytes(f.read(2),"little")
         tmp.append(ans)
     data.append(tmp)
-# for x in range(0,10):
-#     print(data[x])
 data=np.array(data)
-print(data.shape)
-plt.plot(data[:,2])
-plt.plot(data[:,3])
-plt.plot(data[:,4])
+for x in range(0,analog):
+    plt.figure(x+1)
+    plt.plot(data[:,2+x])
+    plt.xlabel('Samples')
+    plt.ylabel(analogchannelname[x]+' ('+unit[x]+')')
+# for y in range(0,analog):
+#     plt.xlabel('Samples')
+#     plt.ylabel(digitalchannelname[y])
+#     plt.figure(x+1)
+#     plt.plot(data[:,2+x])
+
+# plt.xlabel('Samples')
+# plt.ylabel('Value')
+
+# plot1 = plt.figure(1)
+# plt.plot(data[:,2])
+# plot2 = plt.figure(2)
+# plt.plot(data[:,3])
+# plot2 = plt.figure(3)
+# plt.plot(data[:,4])
+
 plt.show()
-
-"""
-a=f.read(4)
-ans=int.from_bytes(a,"little")
-print(type(ans))
-print(ans)
-print(f"A={a}")
-a=f.read(4)
-ans=int.from_bytes(a,"little")
-print(type(ans))
-print(ans)
-print(f"A={a}")
-
-a=f.read(4)
-ans=struct.unpack('<f', f.read(4))
-print(type(ans))
-print(ans)
-print(f"A={a}")
-
-a=f.read(4)
-ans=struct.unpack('<f', a)
-print(type(ans))
-print(ans)
-print(f"A={a}")
-a=f.read(4)
-ans=struct.unpack('<f', a)
-print(type(ans))
-print(ans)
-print(f"A={a}")
-
-
-#print(f"E={e}")
-#sample_no = int(e,16)
-#print(sample_no)
-#struct.unpack('!f', bytes.fromhex(t))
-"""
 f.close()	
